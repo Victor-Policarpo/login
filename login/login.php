d69674d9371fb6ccd6d5ec226d99da63d8cf4d30
Victor-Policarpo <victorpolicarposilva@gmail.com>
2024-09-13 02:08:13 -0300

    teste

--- login/login.php
+++
@@ -1,5 +0,0 @@
-Opa, é apenas um teste de login responsivo. Não esta ligado a algum banco de dados ou outros sites.
-Não se preucupe seus dados não foram enviados a ninguem! nem mesmo ao criador do site pois o arquivo php nao existe para fazer ligação com o banco de dados ou coisas afins. Obrigado por testar meu Login.
-
-
-Feito por: Victor-Policarpo (GitHub)
